643cd460940a6cb7ad6518144ae097c24f751761
Kyle Fennell <kfennell@indeed.com>
2020-01-20 23:08:56 +0900

    docstrings & req modules

--- faceauth/__init__.py
+++ faceauth/__init__.py
@@ -27,7 +27,3 @@ face_auth = oauth.register(
 
 
 from .views import *
-
-# https://www.facebook.com/v5.0/dialog/oauth?response_type=token&display=popup&client_id=170328370854476&redirect_uri=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fexplorer%2Fcallback%3Fmethod%3DGET%26path%3Dme%253Ffields%253Did%252Cname%252Caddress%252Cbirthday%26version%3Dv5.0&auth_type=rerequest&scope=user_birthday%2Cpublic_profile
-# https://graph.accountkit.com/v5.0/dialog/oauth?response_type=code&client_id=170328370854476&redirect_uri=https%3A%2F%2Flocalhost%3A5000%2Fcallback&state=QMP9HS9bY9UdKp50THgYxiWYf9naL8
-# https://localhost:5000/callback?code=AQAJAqrZ6wfYvcyA_0amZ0XwLF2Y1t0n6-yTREVZVVFUCrBH8xSVgwWnBI4Nqem9ozguocioKuQIko4kh4eFczwVwidP4UYq80rJpJV6V9gWQD-1gWqwuadZXPvioqKC_4c-YB1wcRUk3gk9fOq6BG309FyTmslujrotwJoct3UYsPM65zMKPDdBShI4zmoSJrF8c67aVaWqoFSJGTsfcndZLE9JL1tsxNM1Yo2Z9Hye7J1xwDw9QQs7Ekx0O-cND8FuRli08h8-Nmq1cFiKnbUg366c3lTYthDNyKWfdANssjemZaFq2To_WyrlnKEMgASBzmbNSzXZgRJQmczVcpx-&state=4VZZQZ0zNI4tTbufEhQO3sJiKGhEhA#_=_
--- faceauth/views.py
+++ faceauth/views.py
@@ -7,16 +7,19 @@ from .__init__ import app, face_auth
 
 @app.route("/")
 def index():
+    """main landing page, loads index.html"""
     return render_template("index.html")
 
 @app.route('/login')
 def login():
+    """redirects from index to authorization page"""
     redirect_uri = url_for('.callback', _external=True)
     return face_auth.authorize_redirect(redirect_uri)
 
 
 @app.route('/callback')
 def callback():
+    """capture authorization code and exchange for token"""
     token = face_auth.authorize_access_token()
     print(token)
     facebook_id = face_auth.get("https://graph.facebook.com/v5.0/me?").json()["id"]
@@ -29,4 +32,5 @@ def callback():
 
 @app.route('/loggedin')
 def loggedin():
+    """logged in page"""
     return render_template('loggedin.html')
